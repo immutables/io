8bd78d672c8574839f93c3d23ab7ca64becc9964
Eugene Lukash <e.lucash@gmail.com>
2020-11-20 01:36:05 -0500

    binary for ecs

--- up.js
+++ up.js
@@ -1,6 +1,7 @@
 // uses Highlands downloaded into .ext
 // then we include all the dependencies definitions from io/deps.js
-require('./.ext/highlands/')
+//require('./.ext/highlands/')
+require('./.highlands/')
 	.include(() => require('./io/deps.js'))
 	.lib('//io/ecs/sample:pre', {
 		internal: true,
